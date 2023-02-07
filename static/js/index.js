64bf132286a1f0abe0c902810614c3ab286db205
Fernando Garcia Perez <fernando03gar729@gmail.com>
2023-02-07 04:34:38 -0600

    first

---
+++ static/js/index.js
@@ -0,0 +1,58 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+
+//Data science
+function add1() {
+  document.getElementById("infoserv").innerHTML = 
+  '    <div class="uk-animation-toggle;" tabindex="0";> '+
+  '    <div class="uk-text-justify uk-animation-slide-top-medium"> ' +
+    '<h1 class="uk-text-center">Machine Learning</h1>'+" Fundamentally, machine learning (ML) uses specialized algorithms to collect "+
+    "and evaluate input data to predict products whose values are within acceptable ranges. As new data is placed "+
+    "into these ML algorithms, they begin to understand and improve their processes to advance the performance, evolving their ‘intelligence’ over time. "+
+    " At Projexel, we understand the difficulties our clients may face, and we can apply our knowledge to help you turn those challenges into opportunities."+
+    " Our team has expertise in every potential application of machine learning to solve your most complex business problems."+
+    '  </div> </div>';
+}
+
+function add2() {
+  document.getElementById("infoserv").innerHTML = 
+  '    <div class="uk-animation-toggle;" tabindex="0";> '+
+  '    <div class="uk-text-justify uk-animation-slide-top-medium"> ' +
+    '<h1 class="uk-text-center"> NLP </h1>'+"  Natural language processing (NLP) ultimately falls under the wide-ranging category of AI, but also shares essential characteristics "+
+    "of computational linguistics and computer science. Basically, NLP is the process by which machines learn to make sense of human language. Therefore, NLP"+
+    " results in a model that can successfully process text and remove any vagueness within it, a process is known as word sense disambiguation.</p> <p>For achieving"+
+    " the above-mentioned objective, machines need to be trained with textual data. For the purpose of training the machine, first, the textual training data is pre-processed "+
+    "in different ways based on the end objective. The rudimentary preprocessing techniques that are often used are tokenization, stemming, lemmatization, and stop word removal. "+
+    "These techniques which are considered baseline are used in some basic applications like document similarity and topic modeling among others. There are other techniques such "+
+    "as parts-of-speech tagging, named entity recognition that is used in combination with the previously mentioned baseline techniques for more complex applications like document "+
+    "summarization, sentiment analysis, and machine translation.</p> <p>In the current tech-fostered world, Projexel provides a fast and user-friendly service, and a good customer"+
+    " experience is a basic benchmark for a business or organization to succeed. Natural Language Processing enables us to provide that in the best of ways. Intelligent Virtual Assistants "+
+    "help us assist customers in resolving their issues with no human interference. Machine Translation systems help us get past the language barriers and extend our services to diverse "+
+    "communities. Sentiment Analysis will help us sense the mood of the customers towards our services from their reviews or feedback. <span>These are just a few use-case scenarios "+
+    "among many others, and we will always have the flexibility to use NLP to cater to our customized and specific needs.</span>"+
+    '  </div> </div>';
+}
+function add3() {
+  document.getElementById("infoserv").innerHTML = 
+  '    <div class="uk-animation-toggle;" tabindex="0";> '+
+  '    <div class="uk-text-justify uk-animation-slide-top-medium"> ' +
+    '<h1 class="uk-text-center">'+"Artificial Intelligence Consulting And Solutions</h1> Projexel has more than 20 years of expertise in building APIs and software applications with GUIs "+
+    "that leverage artificial intelligence to learn and automate a myriad set of complex daily workflows. We build & configure software products and services to train "+
+    "intelligent recommendation and forecasting systems using state-of-the-art machine learning techniques. We are able to support the requirements set by the customer "+
+    "for image, video & speech processing platforms that enable users to leverage advanced computer vision and natural language processing techniques."+
+    '  </div> </div>';
+}
+function add4() {
+  document.getElementById("infoserv").innerHTML = 
+  '    <div class="uk-animation-toggle;" tabindex="0";> '+
+  '    <div class="uk-text-justify uk-animation-slide-top-medium"> ' +
+    '<h1 class="uk-text-center">Computer vision</h1>'+" Computer vision (CV) is the part of artificial intelligence (AI) that allows computers and "+
+    "systems to develop information from visual media such as digital images and videos and derive appropriate actions or programs "+
+    "based on the information gathered. Thus, artificial intelligence enables computers to consider appropriate actions, while CV enables "+
+    "computers to perceive and identify an individual's visual senses through vision. By utilizing a variety of algorithmic models to find "+
+    "out the perspective of the visual media that it is consuming. Over time, when a sufficient amount of data is processed through an algorithmic model, "+
+    "the computer begins to discern visual data. The entire process is completely independent of personal intervention during media evaluation." +
+    '  </div> </div>';
+}
